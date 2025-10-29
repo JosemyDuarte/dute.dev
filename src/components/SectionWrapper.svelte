e39d6ff4176b00980d6ec1a89888d3613117419f
josemy.duarte <duartejosemy@gmail.com>
2025-10-29 08:52:37 +0100

    refactor: add TypeScript types to SectionWrapper

--- src/components/SectionWrapper.svelte
+++ src/components/SectionWrapper.svelte
@@ -1,8 +1,12 @@
-<script>
-	export let id;
+<script lang="ts">
+	interface Props {
+		id: string;
+	}
+
+	let { id }: Props = $props();
 </script>
 
-<section {id} class={'flex min-h-screen flex-col px-4 '}>
+<section {id} class="flex min-h-screen flex-col px-4">
 	<div class="mx-auto flex w-full max-w-[1400px] flex-1 flex-col">
 		<slot />
 	</div>
